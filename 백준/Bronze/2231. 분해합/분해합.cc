5d49f052c6340047c7dbf91b1af066eea0446041
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2023-10-05 11:22:04 +0900

    [Bronze II] Title: 분해합, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int findConstructor(int n) {
+	int sum;
+
+	for (int i = 1; i < n; i++) {
+		int temp = i;
+		sum = i;
+
+		while (temp > 0) {
+			sum += temp % 10;
+			temp /= 10;
+		}
+
+		if (sum == n) {
+			return i;
+		}
+	}
+
+	return 0;
+}
+
+int main() {
+	
+	int n;
+	cin >> n;
+	cout << findConstructor(n);
+
+	return 0;
+}
