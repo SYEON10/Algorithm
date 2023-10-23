1a351681dd724feeb18684d278e9517aa4bd0571
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2023-10-23 20:07:25 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,3 +1,6 @@
+//
+// Created by syeon on 2023-10-23.
+//
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,24 +13,19 @@ int main() {
     cin >> n >> k;
 
     vector<pair<int, int>> products(n + 1, {0, 0});
-    vector<vector<int>> DP(n + 1, vector<int>(k + 1, 0));
+    vector<int> DP2(k+1);
 
     for(int i = 1; i <= n; i++){
         cin >> products[i].first >> products[i].second;
     }
 
-    for(int i = 1; i <= n; i++){
-        //이전 DP 복사
-        for(int j = 1; j < products[i].first; j++){
-            DP[i][j] = DP[i - 1][j];
-        }
-        //연산-업데이트
-        for(int j = products[i].first; j <= k; j++){
-            DP[i][j] = max(DP[i - 1][j], products[i].second + DP[i - 1][j - products[i].first]);
+    for(int i = 1; i <= n; i++){ //물건 넣기
+        for(int j = k; j >= products[i]. first; j--){
+            DP2[j] = max(DP2[j], products[i].second + DP2[j - products[i].first]);
         }
     }
 
-    cout << DP[n][k];
+    cout << DP2[k];
 
     return 0;
 }
