4d921caa092b22586b0545c49bc3716fd0f63a8f
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2024-01-23 09:57:53 +0900

    [Silver II] Title: 쇠막대기, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,39 +1,30 @@
 #include <iostream>
 #include <stack>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
     string line;
-    int count = 0;
-    bool laser = true;
-    stack<char> temp;
+    int stack = 0;
+    int ans = 0;
+    char pch;
 
     cin >> line;
 
     for(int i = 0; i < line.size(); i++){
-        char c = line[i];
-        if(c == '('){
-            laser = true;
-            temp.push(c);
+        if(line[i] == '('){
+            stack++;
+            pch = '(';
         }
         else{
-            temp.pop();
-
-            if(laser){
-                count += temp.size();
-            }
-            else{
-                count++;
-            }
-
-            laser = false;
+            stack--;
+            ans += (pch == '(') ? stack : 1;
+            pch = ')';
         }
     }
 
-    cout << count;
+    cout << ans;
 
     return 0;
 }
