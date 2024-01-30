caedc14939c9bbf8c72b617d79f425dd49e6b83b
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2024-01-30 23:14:24 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> printer;
+vector<bool> checker;
+vector<int> nums;
+
+void BackTracking(int idx, int start){
+    if(idx == m){
+        for(int i = 0; i < m; i++){
+            cout << printer[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int prev = 0;
+    for(int i = start; i < n;i++){
+        if(!checker[i] && prev != nums[i]){
+            prev = nums[i];
+            printer[idx] = nums[i];
+            checker[i] = true;
+            BackTracking(idx + 1, i);
+            checker[i] = false;
+        }
+    }
+}
+
+int main() {
+
+    cin >> n >> m;
+
+    nums.assign(n, 0);
+    checker.assign(n, false);
+    printer.assign(m, 0);
+
+    for(auto& num : nums){
+        cin >> num;
+    }
+
+    sort(nums.begin(), nums.end());
+
+    BackTracking(0, 0);
+
+    return 0;
+}
