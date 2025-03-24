1a522742dae0cedf0e69f7117c449b9490421090
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-03-24 12:44:39 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1632 ms, Memory: 48420 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -18,7 +18,8 @@ int main()
         priority_queue<int> max_pq;
         priority_queue<int, std::vector<int>, std::greater<int>> min_pq;
         
-        unordered_map<int, int> checker;
+        unordered_map<int, int> max_map;
+        unordered_map<int, int> min_map;
 
         while(n--){
             char cmd;
@@ -28,39 +29,44 @@ int main()
             if(cmd == 'I'){
                 max_pq.push(param);
                 min_pq.push(param);
-                checker[param]++;
             }
             if(cmd == 'D'){
                 if(param == 1){
-                    while(!max_pq.empty() && checker[max_pq.top()] == 0){
+                    while(!max_pq.empty() && max_map[max_pq.top()] != 0){
+                        max_map[max_pq.top()]--;
                         max_pq.pop();
                     }
 
                     if(max_pq.empty()) continue;
-                    checker[max_pq.top()]--;
+
+                    min_map[max_pq.top()]++;
                     max_pq.pop();
                 }
                 else{
-                    while(!min_pq.empty() && checker[min_pq.top()] == 0){
+                    while(!min_pq.empty() && min_map[min_pq.top()] != 0){
+                        min_map[min_pq.top()]--;
                         min_pq.pop();
                     }
 
                     if(min_pq.empty()) continue;
-                    checker[min_pq.top()]--;
+
+                    max_map[min_pq.top()]++;
                     min_pq.pop();
                 }
             }
         }
 
-        while(!max_pq.empty() && checker[max_pq.top()] == 0){
+        while(!max_pq.empty() && max_map[max_pq.top()] != 0){
+            max_map[max_pq.top()]--;
             max_pq.pop();
         }
 
-        while(!min_pq.empty() && checker[min_pq.top()] == 0){
+        while(!min_pq.empty() && min_map[min_pq.top()] != 0){
+            min_map[min_pq.top()]--;
             min_pq.pop();
         }
 
-        if(max_pq.empty()) cout << "EMPTY" << '\n';
+        if(max_pq.empty() || min_pq.empty()) cout << "EMPTY" << '\n';
         else cout << max_pq.top() << ' ' << min_pq.top() << '\n';
     }
     return 0;
