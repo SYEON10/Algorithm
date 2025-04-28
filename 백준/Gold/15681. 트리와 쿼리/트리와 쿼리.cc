0532fb4cfd1ceb4ca944e230377c87cd42ac1a64
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-04-28 16:40:12 +0900

    [Gold V] Title: 트리와 쿼리, Time: 68 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> tree;
+vector<int> cnt;
+
+int DFS(int node, int parent){
+    if(cnt[node] != 0){
+        return cnt[node];
+    }
+
+    for(int i = 0; i < tree[node].size(); i++){
+        if(tree[node][i] == parent) continue;
+        cnt[node] += DFS(tree[node][i], node);
+    }
+
+    return cnt[node] += 1;
+}
+
+int main()
+{
+    cin.tie(0) -> sync_with_stdio(0);
+    
+    int n, r, q;
+    cin >> n >> r >> q;
+
+    tree.assign(n + 1, vector<int>());
+    cnt.assign(n + 1, 0);
+
+    for(int i = 0; i < n - 1; i++){
+        int node1, node2;
+        cin >> node1 >> node2;
+
+        tree[node1].push_back(node2);
+        tree[node2].push_back(node1);
+    }
+
+    DFS(r, 0);    
+
+    for(int i = 0; i < q; i++){
+        int query = 0;
+        cin >> query;
+
+        cout << cnt[query]<< '\n';
+    }
+
+    return 0;
+}
