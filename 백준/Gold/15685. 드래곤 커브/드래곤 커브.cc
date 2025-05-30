a4d4cb79ac100bf215401725f03e30cecc4254a4
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-05-30 14:23:59 +0900

    [Gold III] Title: 드래곤 커브, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
@@ -7,46 +7,45 @@ using namespace std;
 const int END = 100;
 const int MAX_GEN = 10;
 
-struct curve {
+struct point {
     int x;
     int y;
 };
 
-vector<vector<bool>> dragonCurve;
+vector<vector<bool>> board;
 
-curve GetNextCurve(curve c, int d) {
+point getNextPoint(point c, int d) {
     switch (d) {
     case 0:
-        return curve{ c.x + 1, c.y };
+        return point{ c.x + 1, c.y };
     case 1:
-        return curve{ c.x, c.y - 1 };
+        return point{ c.x, c.y - 1 };
     case 2:
-        return curve{ c.x - 1, c.y };
+        return point{ c.x - 1, c.y };
     case 3:
-        return curve{ c.x, c.y + 1 };
+        return point{ c.x, c.y + 1 };
     }
-    return curve{ 0, 0 };
+    return point{ 0, 0 };
 }
 
-int Rotate(int direction) {
+int rotate(int direction) {
     return (direction + 1) % 4;
 }
 
-void DragonCurve(curve data, int direction, int generation) {
-    vector<int> route;
-    route.push_back(direction);
-    dragonCurve[data.x][data.y] = true;
+void dragonCurve(point data, int direction, int generation) {
+    vector<int> route = { direction };
 
-    data = GetNextCurve(data, direction);
-    dragonCurve[data.x][data.y] = true;
+    board[data.x][data.y] = true;
+    data = getNextPoint(data, direction);
+    board[data.x][data.y] = true;
 
     for (int i = 1; i <= generation; i++) {
         int current_route = route.size();
         for (int j = current_route - 1; j >= 0; j--) {
-            int d = Rotate(route[j]);
+            int d = rotate(route[j]);
             route.push_back(d);
-            data = GetNextCurve(data, d);
-            dragonCurve[data.x][data.y] = true;
+            data = getNextPoint(data, d);
+            board[data.x][data.y] = true;
         }
     }
 }
@@ -56,19 +55,19 @@ int main()
     int n;
     cin >> n;
 
-    dragonCurve.assign(END + 1, vector<bool>(END + 1, false));
+    board.assign(END + 1, vector<bool>(END + 1, false));
 
     for (int i = 0; i < n; i++) {
         int x, y, d, g;
         cin >> x >> y >> d >> g;
-        DragonCurve(curve{ x, y }, d, g);
+        dragonCurve(point{ x, y }, d, g);
     }
 
     int count = 0;
 
     for (int i = 0; i < END; i++) {
         for (int j = 0; j < END; j++) {
-            if (dragonCurve[i][j] && dragonCurve[i + 1][j] && dragonCurve[i][j + 1] && dragonCurve[i + 1][j + 1]) {
+            if (board[i][j] && board[i + 1][j] && board[i][j + 1] && board[i + 1][j + 1]) {
                 count++;
             }
         }
