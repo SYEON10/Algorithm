1815f33571ae8d545790c4dad48dee38acd0a2af
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-06-08 18:00:43 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.02 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.scala
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.scala
@@ -1,10 +1,5 @@
 object Solution {
     def solution(n: Int): String = {
-        val ans = (1 to n).map{
-            case i => 
-                if(i % 2 == 1) "수";
-                else "박";
-        }.mkString("")
-        return ans
+        "수박" * (n / 2) + "수" * (n % 2)
     }
 }
