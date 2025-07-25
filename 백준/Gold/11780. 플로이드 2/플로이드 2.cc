e571e408f17efb512a12b551a0742bfbb79d3381
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-07-25 15:00:50 +0900

    [Gold II] Title: 플로이드 2, Time: 28 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.cc
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.cc
@@ -7,45 +7,28 @@ using namespace std;
 
 const int MAX_COST = 1e5;
 
-vector<vector<int>> DP;
-vector<vector<int>> track;
-vector<int> ans;
-
-void BackTrack(int from, int to) {
-    if (track[from][to] == -1) {
-        return;
-    }
-    if (track[from][to] == 0) {
-        ans.push_back(from);
-        return;
-    }
-
-    BackTrack(from, track[from][to]);
-    BackTrack(track[from][to], to);
-}
-
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int n, m;
     cin >> n >> m;
 
-    DP.assign(n + 1, vector<int>(n + 1, MAX_COST));
-    track.assign(n + 1, vector<int>(n + 1, -1));
+    vector<vector<int>> DP(n + 1, vector<int>(n + 1, MAX_COST));
+    vector<vector<int>> track(n + 1, vector<int>(n + 1, 0));
 
     for (int i = 0; i < m; i++) {
         int from, to, cost;
         cin >> from >> to >> cost;
         DP[from][to] = min(DP[from][to], cost); //중복 경로가 있으면 min 처리 필요
-        track[from][to] = 0;
+        track[from][to] = to;
     }
 
     for (int i = 1; i <= n; i++) {
         DP[i][i] = 0;
-        track[i][i] = 0;
     }
 
     //DP[from][to] = from에서 to까지 가는 최단경로
+    //track[from][to] = from부터 to까지 가는 길에서 from 바로 다음에 와야 할 노드
 
     for (int k = 1; k <= n; k++) {
         for (int from = 1; from <= n; from++) {
@@ -53,7 +36,7 @@ int main() {
                 if (DP[from][k] == MAX_COST || DP[k][to] == MAX_COST) continue;
                 if (DP[from][to] > DP[from][k] + DP[k][to]) {
                     DP[from][to] = DP[from][k] + DP[k][to];
-                    track[from][to] = k;
+                    track[from][to] = track[from][k];
                 }
             }
         }
@@ -69,21 +52,23 @@ int main() {
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if (i == j) {
+            if (i == j || track[i][j] == 0) {
                 cout << 0 << '\n';
                 continue;
             }
-            BackTrack(i, j);
-            if (ans.size() == 0) {
-                cout << 0 << '\n';
-                continue;
+
+            vector<int> path;
+            path.push_back(i);
+
+            for(int from = i; from != j; from = track[from][j]){
+                path.push_back(track[from][j]);
             }
-            cout << ans.size() + 1 << ' ';
-            for (auto k : ans) {
-                cout << k << ' ';
+
+            cout << path.size() << ' ';
+            for(auto p : path){
+                cout << p << ' ';
             }
-            cout << j << '\n';
-            ans.clear();
+            cout << '\n';
         }
     }
     return 0;
