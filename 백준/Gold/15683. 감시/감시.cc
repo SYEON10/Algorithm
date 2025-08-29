84b1289d7a043413b3fe4b9a0d5b063f77f45dcc
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-08-29 15:44:55 +0900

    [Gold III] Title: 감시, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,117 +1,116 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <cmath>
 #include <algorithm>
 
 using namespace std;
 
-struct CCTV {
-    int r, c;
-    int num;
-};
+//N, M이 매우 작은 시뮬레이션 문제
 
-struct Install {
-    CCTV tv;
+//1. 완전탐색 - 모든 CCVT 방향 경우의 수를 시뮬레이션 -> 4의 8승 => 2의 16승 : 가능
+//2. ...?
+
+struct point{
+    int r;
+    int c;
+    int type;
     int dir;
 };
 
-vector<vector<int>> matrix;
-vector<CCTV> cctv;
-vector<int> direction;
-int ans = INT32_MAX;
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
+int n, m;
+vector<vector<int>> board;
+vector<point> cctv;
+int result = INT32_MAX;
+int obj_count = 0;
 
-void Check(int ct, int dir, vector<vector<int>>& board) {
-    int i = 1;
-    while (true) {
-        int nr = cctv[ct].r + dy[dir] * i;
-        int nc = cctv[ct].c + dx[dir] * i;
-
-        if (board[nr][nc] == 6) return;
-        if (board[nr][nc] == 0) board[nr][nc] = -1;
-        i++;
-    }
-}
-
-int Calculate() {
-    vector<vector<int>> board = matrix;
-    for (int i = 0; i < cctv.size(); i++) {
-        switch (cctv[i].num) {
-        case 1:
-            Check(i, direction[i], board);
-            break;
-        case 2:
-            Check(i, direction[i], board);
-            Check(i, (direction[i] + 2) % 4, board);
-            break;
-        case 3:
-            Check(i, direction[i], board);
-            Check(i, (direction[i] + 1) % 4, board);
-            break;
-        case 4:
-            Check(i, direction[i], board);
-            Check(i, (direction[i] + 1) % 4, board);
-            Check(i, (direction[i] + 2) % 4, board);
-            break;
-        case 5:
-            Check(i, 0, board);
-            Check(i, 1, board);
-            Check(i, 2, board);
-            Check(i, 3, board);
-            break;
-        }
-    }
+//0 1 2 3
+int Direction(int pr, int pc, int dir, vector<vector<int>>& b){
     int count = 0;
+    int dr = 0, dc = 0;
+    if(dir % 2 == 0) dr = dir - 1;
+    else dc = dir - 2;
 
-    for (auto& row : board) {
-        for (auto& val : row) {
-            if (val == 0) {
-                count++;
-            }
+    for(int r = pr, c = pc; r < n && r >= 0 && c < m && c >= 0; r += dr, c += dc){
+        if(b[r][c] == 6) break;
+        if(b[r][c] == 0) {
+            count++;
+            b[r][c] = -1;
         }
     }
-
     return count;
 }
 
-//CCTV의 방향에 따라 백트래킹
-//cctv 인덱스를 기준으로, 총 4방향으로 돌아감
-void BackTracking(int ct) {
-    if (ct >= cctv.size()) {
-        ans = min(ans, Calculate());
+//시뮬레이션
+int Simulation(){
+    vector<vector<int>> b = board;
+    int count = 0;
+    for(point p : cctv){
+        switch(p.type){
+            case 1:
+                count += Direction(p.r, p.c, p.dir, b);
+                break;
+            case 2:
+                count += Direction(p.r, p.c, p.dir, b);
+                count += Direction(p.r, p.c, (p.dir + 2) % 4, b);
+                break;
+            case 3:
+                count += Direction(p.r, p.c, p.dir, b);
+                count += Direction(p.r, p.c, (p.dir + 1) % 4, b);
+                break;
+            case 4:
+                count += Direction(p.r, p.c, p.dir, b);
+                count += Direction(p.r, p.c, (p.dir + 1) % 4, b);
+                count += Direction(p.r, p.c, (p.dir + 2) % 4, b);
+                break;
+            case 5:
+                count += Direction(p.r, p.c, p.dir, b);
+                count += Direction(p.r, p.c, (p.dir + 1) % 4, b);
+                count += Direction(p.r, p.c, (p.dir + 2) % 4, b);
+                count += Direction(p.r, p.c, (p.dir + 3) % 4, b);
+                break;
+        }
+    }
+    return n * m - (count + obj_count);
+}
+
+//모든 CCTV에 대한 방향 경우의 수 계산
+void BackTracking(int c){
+    if(c == cctv.size()){
+        result = min(result, Simulation());
         return;
     }
 
-    for (int i = 0; i < 4; i++) {
-        direction[ct] = i;
-        BackTracking(ct + 1);
+    for(int i = 0; i <= 3; i++){
+        cctv[c].dir = i;
+        BackTracking(c + 1);
     }
 }
 
-int main()
-{
-    cin.tie(0)->sync_with_stdio(0);
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
 
-    int n, m;
+    //완탐 가능은 하지만 최적화로 풀랬으니 완탐은 X
+    //CCTV의 방향을 결정 -> 사각지대를 최소화
+    //모든 경우의 수에 대해서 탐색...
+    //모든 CCTV에 대해서 방향을 설정 후 시뮬레이션?
+    
     cin >> n >> m;
-
-    matrix.assign(n + 2, vector<int>(m + 2, 6));
-
-    for (int r = 1; r <= n; r++) {
-        for (int c = 1; c <= m; c++) {
-            cin >> matrix[r][c];
-            if (matrix[r][c] > 0 && matrix[r][c] < 6) {
-                cctv.push_back({ r, c, matrix[r][c] });
+    board.assign(n, vector<int>(m));
+    for(int r = 0; r < n; r++){
+        for(int c = 0; c < m; c++){
+            cin >> board[r][c];
+            if(board[r][c] != 0){
+                obj_count++;
+            }
+            if(board[r][c] != 6 && board[r][c] != 0){
+                cctv.push_back(point{r, c, board[r][c], 0});
             }
         }
     }
-
-    direction.resize(cctv.size());
-
     BackTracking(0);
 
-    cout << ans;
+    cout << result;
 
     return 0;
 }
