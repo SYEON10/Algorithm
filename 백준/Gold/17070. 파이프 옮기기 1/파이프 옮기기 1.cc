19152d842a253fcbb01159354a47667a47bacaa8
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-09-02 15:30:23 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,42 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-int main()
-{
+int main() {
     cin.tie(0) -> sync_with_stdio(0);
 
     int n;
     cin >> n;
 
-    vector<vector<int>> matrix(n + 1, vector<int>(n + 1, 0));
-    //0 가로 1 세로 2 대각선
-    vector<vector<vector<int>>> DP(n + 1, vector<vector<int>>(n + 1, vector<int>(3, 0)));
+    int board[n + 1][n + 1];
+    int DP[3][n + 1][n + 1]; //0 - 가로, 1 - 세로, 2 - 대각선
+
+    memset(board, 0, sizeof(board));
+    memset(DP, 0, sizeof(DP));
 
     for(int r = 1; r <= n; r++){
         for(int c = 1; c <= n; c++){
-            cin >> matrix[r][c];
+            cin >> board[r][c];
         }
     }
 
-    DP[1][2][0] = 1;
+    DP[0][1][2] = 1;
 
     for(int r = 1; r <= n; r++){
         for(int c = 2; c <= n; c++){
-            if(matrix[r][c] == 1) continue;
-
-            DP[r][c][0] += DP[r][c - 1][0] + DP[r][c - 1][2];
-            DP[r][c][1] += DP[r - 1][c][1] + DP[r - 1][c][2];
-
-            if(!(matrix[r][c-1] == 1 || matrix[r-1][c] == 1)){
-                DP[r][c][2] += DP[r - 1][c - 1][0] + DP[r - 1][c - 1][1] + DP[r - 1][c - 1][2];
-            }
+            if(board[r][c] == 1) continue;
+            if(r == 1 && c == 2) continue;
+            DP[0][r][c] = DP[0][r][c - 1] + DP[2][r][c - 1];
+            DP[1][r][c] = DP[1][r - 1][c] + DP[2][r - 1][c];
+            if(board[r - 1][c] == 1 || board[r][c - 1] == 1) continue;
+            DP[2][r][c] = DP[0][r - 1][c - 1] + DP[1][r - 1][c - 1] + DP[2][r - 1][c - 1];
         }
     }
 
-    cout << DP[n][n][0] + DP[n][n][1] + DP[n][n][2];
+    cout << DP[0][n][n] + DP[1][n][n] + DP[2][n][n];
 
     return 0;
 }
