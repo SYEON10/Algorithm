920b3201c35bcf641837a33467d6a08cd57b0530
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-10-04 22:07:35 +0900

    [Bronze III] Title: 사격 내기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27960. 사격 내기/사격 내기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    int A, B;
+    cin >> A >> B;
+    int C = A ^ B;
+    cout << C;
+
+    return 0;
+}
