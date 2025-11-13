a5c81a2333fb226d33d799b72b7c40a3b4a2eb1b
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-11-13 20:08:14 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,52 +1,55 @@
 #include <iostream>
-#include <string>
-#include <map>
+#include <vector>
+#include <cmath>
 #include <algorithm>
+#include <climits>
+#include <stack>
 
 using namespace std;
 
-//팰린드롬이 만들어질 수 있는 조건
-//1. 홀수 개인 문자가 없거나 1개만 있어야 한다.
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
 
-//위 조건을 만족하는 문자열로 팰린드롬을 만드는 방법
-//1. 각 문자열의 수를 파악한다.
-//2. 사전순으로 빠른 문자열의 절반을 전부 나열해나간다. 단, 홀수개는 무조건 마지막에 온다.
-//3. 나머지 절반의 문자열을 뒤집어 그 뒤에 붙인다.
+    string str;
+    cin >> str;
 
-//key-value 로 key에 알파벳 저장하고 그 개수를 value에 저장한다.
-
-int main(){
-
-    map<char, int> dict;
-    char c, odd = '\0';
-    string answer, submit;
-
-    while(cin >> c){
-        if(c == '!')
-            break;
-        dict[c]++;
+    vector<int> alphabet(26, 0);
+    for(char c : str){
+        alphabet[c - 'A']++;
     }
 
-    for(auto in : dict){
-        if(in.second % 2 != 0 && odd != '\0'){
-            cout << "I'm Sorry Hansoo";
-            return 0;
+    char add;
+    bool isAdd = false;
+
+    stack<char> stk;
+    vector<char> res;
+
+    for(int i = 0; i < 26; i++){
+        char c = 'A' + i;
+        if(alphabet[i] & 1){
+            if(isAdd){
+                cout << "I'm Sorry Hansoo";
+                return 0;
+            }
+            isAdd = true;
+            add = c;
         }
-        if(in.second % 2 != 0){ //홀수면
-            odd = in.first;
-        }
-        for(int i = 0; i < in.second / 2; i++){
-            answer += in.first;
+        for(int j = 0; j < alphabet[i] / 2; j++){
+            res.push_back(c);
+            stk.push(c);
         }
     }
 
-    submit += answer;
-    if(odd != '\0')
-        submit += odd;
-    reverse(answer.begin(), answer.end());
-    submit += answer;
+    for(char c : res){
+        cout << c;
+    }
 
-    cout << submit;
+    if(isAdd) cout << add;
+
+    while(!stk.empty()){
+        cout << stk.top();
+        stk.pop();
+    }
 
     return 0;
 }
