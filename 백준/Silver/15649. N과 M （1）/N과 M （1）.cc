9b74a578c1c1779dc8325678b33923511d66bc60
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-11-21 18:42:15 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,45 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
-vector<int> table;
-vector<bool> check;
+//중복 없이 M개를 고른 수열 => 순열 구하기!
+
 int n, m;
+vector<bool> visited;
+vector<int> arr;
 
-void print(){
-    for(int i = 1; i <=m ; i++){
-        cout << table[i] << ' ';
-    }
-    cout << '\n';
-}
-
-void BackTracking(int index){
-    if(index == m){
-        print();
+void back(int cnt){
+    if(cnt == m){
+        for(int i : arr){
+            cout << i << ' ';
+        }
+        cout << '\n';
         return;
     }
 
-    for(int num = 1; num <= n; num++){
-        if(table[index] == num || check[num]){
-            continue;
-        }
-        table[index + 1] = num;
-        check[num] = true;
-        BackTracking(index + 1);
-        check[num] = false;
+    for(int i = 1; i <= n; i++){
+        if(visited[i]) continue;
+        visited[i] = true;
+        arr[cnt] = i; 
+        back(cnt + 1);
+        visited[i] = false;
     }
 }
 
-int main()
-{
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+
     cin >> n >> m;
 
-    table.assign(m + 1, 0);
-    check.assign(m + 1, false);
+    visited.assign(n + 1, false);
+    arr.assign(m, 0);
 
-    BackTracking(0);
+    back(0);
 
     return 0;
 }
